7516453d4734717fb4568afa799727f86ef12bf5
cyb3renemy <100950529+cyb3renemy@users.noreply.github.com>
2023-04-05 22:55:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,68 +1,53 @@
 import os
 import pandas as pd
 from telegram import ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# створюємо об'єкт бота та підключаємося до Telegram API
-updater = Updater(token=os.environ['T5811427895:AAGqu0l6gOaX76KCXfWd0vQb6MrUU79g5qk'])
-dispatcher = updater.dispatcher
+# читаємо файл бази даних
+if os.path.exists("database.txt"):
+    df = pd.read_csv("database.txt", header=None, names=["text"])
+    existing_texts = set(df.text)
+else:
+    existing_texts = set()
 
-# створюємо базу даних для зберігання унікальних строчок
-db_path = 'db.csv'
-if not os.path.exists(db_path):
-    pd.DataFrame(columns=['string']).to_csv(db_path, index=False)
+# обробник команди /start
+def start_handler(update, context):
+    # створюємо клавіатуру з однією кнопкою
+    button = KeyboardButton("roblox.com")
+    reply_markup = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+    
+    # відправляємо повідомлення користувачу з клавіатурою
+    update.message.reply_text("Натисніть кнопку, щоб почати", reply_markup=reply_markup)
 
-# функція для збереження унікальних строчок
-def save_unique_strings(strings):
-    db = pd.read_csv(db_path)
-    new_strings = [s for s in strings if s not in db['string'].values]
-    if new_strings:
-        new_db = pd.concat([db, pd.DataFrame({'string': new_strings})], ignore_index=True)
-        new_db.to_csv(db_path, index=False)
+# обробник повідомлень з файлами
+def file_handler(update, context):
+    file = update.message.document
+    if file.mime_type != "text/plain":
+        update.message.reply_text("Будь ласка, надішліть файл у форматі .txt")
+        return
+    
+    # зчитуємо файли і порівнюємо з базою
+    text = file.get_file().download_as_string().decode("utf-8")
+    new_texts = set(text.strip().split("\n")) - existing_texts
+    
+    if not new_texts:
+        update.message.reply_text("Всі стрічки з файлу вже є в базі даних")
+        return
+    
+    # додаємо унікальні строчки в базу
+    new_df = pd.DataFrame({"text": list(new_texts)})
+    df = pd.concat([df, new_df], ignore_index=True)
+    df.to_csv("database.txt", index=False, header=False)
+    
+    # створюємо файл з унікальними строчками та відправляємо його користувачу
+    unique_text = "\n".join(sorted(new_texts))
+    file_bytes = unique_text.encode("utf-8")
+    update.message.reply_document(document=file_bytes, filename="unique.txt")
 
-# функція для обробки команди /start
-def start(update, context):
-    # створюємо кнопку з посиланням на roblox.com
-    button = KeyboardButton('roblox.com')
-    # створюємо клавіатуру з цією кнопкою
-    keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
-    # надсилаємо повідомлення з клавіатурою
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Натисніть кнопку, щоб отримати посилання на roblox.com",
-                             reply_markup=keyboard)
-
-# функція для обробки файлів, які надіслав користувач
-def handle_document(update, context):
-    # отримуємо файл
-    file = context.bot.get_file(update.message.document.file_id)
-    # читаємо файл
-    content = file.download_as_bytearray()
-    strings = content.decode().split('\n')
-    # зберігаємо унікальні строчки в базу даних та надсилаємо їх користувачеві
-    save_unique_strings(strings)
-    unique_strings = pd.read_csv(db_path)['string'].tolist()
-    unique_content = '\n'.join(unique_strings)
-    context.bot.send_document(chat_id=update.effective_chat.id,
-                              document=unique_content.encode(),
-                              filename='unique_strings.txt')
 # реєструємо обробники повідомлень та запускаємо бота
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
-
-# додаємо обробник для кнопки "roblox.com"
-def handle_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="https://www.roblox.com/")
-dispatcher.add_handler(CallbackQueryHandler(handle_button, pattern='roblox.com'))
-
-# запускаємо бота
+updater = Updater(token='5811427895:AAGqu0l6gOaX76KCXfWd0vQb6MrUU79g5qk')
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start_handler))
+dispatcher.add_handler(MessageHandler(Filters.document, file_handler))
 updater.start_polling()
-# функція для обробки команди /start
-def start(update, context):
-    # створюємо кнопку з посиланням на roblox.com
-    button = KeyboardButton('roblox.com', callback_data='roblox.com')
-    # створюємо клавіатуру з цією кнопкою
-    keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
-    # надсилаємо повідомлення з клавіатурою
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Натисніть кнопку, щоб отримати посилання на roblox.com",
-                             reply_markup=keyboard)
+updater.idle()
